62665f442d9211f4be86aa311a56de2ff9e0c480
cdwest2 <cdwest2@asu.edu>
2019-04-11 14:51:43 -0700

    Update TaskList.java

--- todolist/TaskList.java
+++ todolist/TaskList.java
@@ -3,11 +3,6 @@ import java.util.*;
 //Will manage the list of tasks
 public class TaskList 
 {
-	private ArrayList<Task> list;
-	
-	void add(Task task)
-	{
-		list.add(task);
-	}
+	//will be a linked list
 	
 }
